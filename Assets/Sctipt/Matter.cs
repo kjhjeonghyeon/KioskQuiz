1b4820c24d1fa73d9c2a588e94142bd57c1b620c
kjhjeonghyeon <80999210+kjhjeonghyeon@users.noreply.github.com>
2024-11-14 08:35:33 +0900

    키오스크OX퀴즈 프로토타입제작

---
+++ Assets/Sctipt/Matter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+
+public class Matter : MonoBehaviour
+{
+    //각 인풋필드에 글씨를 쓰고 확인을 누르면 문제가 자동입력된다
+    public TMP_InputField InputO;
+    public TMP_InputField InputX;
+
+    TMP_Text[] Quiztext;
+    int countX;
+    int countO;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {   //input을 통해 문제 받기
+        //문제 리스트만들기 (인스턴트)
+        //문제리스트로 게임 만들기
+    }
+
+    public void InPutX()
+    {
+        countX++;
+        Quiztext[countX] = InputX.GetComponent<TMP_Text>();
+    }
+    public void InPutO()
+    {
+        countO++;
+        Quiztext[countO] = InputO.GetComponent<TMP_Text>();
+    }
+}
